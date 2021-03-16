32681dc7f262adfad58f8822954228cb2bff31c5
julienblv <julien.blavette31@gmail.com>
2021-03-16 13:05:53 +0100

    Setters et getters du Modele_Julien

--- src/calc/Modele.java
+++ src/calc/Modele.java
@@ -2,4 +2,31 @@ package calc;
 
 public class Modele {
 
+	private double nombre1 = 0.;
+	private double nombre2 = 0.;
+
+	Modele()
+	{
+		
+	}
+	
+	public double getNombre1()
+	{
+		return nombre1;
+	}
+
+	public void setNombre1(double nombre1)
+	{
+		this.nombre1 = nombre1;
+	}
+
+	public double getNombre2()
+	{
+		return nombre2;
+	}
+
+	public void setNombre2(double nombre2)
+	{
+		this.nombre2 = nombre2;
+	}
 }
