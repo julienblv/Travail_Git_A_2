ab169fe741823d7aa473f48c87b64c971ea7fd8b
julienblv <julien.blavette31@gmail.com>
2021-03-17 15:41:19 +0100

    Fin de la partie addition_Julien

--- src/calc/Controlleur.java
+++ src/calc/Controlleur.java
@@ -7,11 +7,17 @@ import javax.swing.JTextField;
 
 public class Controlleur {
 	
-	Controlleur(Modele _modele, JTextField _champDeSaisie)
+	Modele modele = new Modele();
+	JTextField champDeSaisie;
+	
+	double operant_Un;
+	double operant_Deux;
+	
+	boolean addition ;
+	
+	Controlleur(JTextField _champDeSaisie)
 	{
-		//  insertion de Champ saisie et modèle pour les ré-utiliser
-		Modele modele = _modele;
-		JTextField champDeSaisie = _champDeSaisie;
+		champDeSaisie = _champDeSaisie;
 	}
 	
 	public void actionPerformed(ActionEvent e)
@@ -20,7 +26,36 @@ public class Controlleur {
 		
 		if (sourceE instanceof JButton)
 		{
+			JButton bouton_click = (JButton) sourceE;
+			String libelle_bouton_click = bouton_click.getText();
 			
+			if (libelle_bouton_click.equals("+"))
+			{
+				// récupération de la variable du champ pour 1er chiffre et affichage dans la barre
+				String texte = champDeSaisie.getText();
+				texte += libelle_bouton_click;
+				texte = texte.replace("+", "");
+				String texte2 = texte.replace("=", "");
+				
+				//parsing
+				double num1 = Double.parseDouble(texte);
+				double num2 = Double.parseDouble(texte2);
+				// Utilisation du modele
+				modele.setNombre1(num1);
+				modele.setNombre2(num2);
+				
+				champDeSaisie.setText("");
+				// c'est une addition
+				Boolean addition = true;
+			}
+			
+			if(addition == true) 
+			{
+				operant_Un = modele.getNombre1();
+				operant_Deux = modele.getNombre2();
+				double Res = modele.addition();
+				champDeSaisie.setText("Resultat : "  + Res);
+			}
 		}
 	}
 }
