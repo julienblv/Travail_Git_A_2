4f018c85bf67006c49c3f28b5295d2c27e9f71d9
julienblv <julien.blavette31@gmail.com>
2021-03-17 11:40:29 +0100

    Commencement du Controlleur_Julien

--- src/calc/Controlleur.java
+++ src/calc/Controlleur.java
@@ -1,5 +1,26 @@
 package calc;
 
-public class Controlleur {
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Controlleur {
+	
+	Controlleur(Modele _modele, JTextField _champDeSaisie)
+	{
+		//  insertion de Champ saisie et modèle pour les ré-utiliser
+		Modele modele = _modele;
+		JTextField champDeSaisie = _champDeSaisie;
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		Object sourceE = e.getSource();
+		
+		if (sourceE instanceof JButton)
+		{
+			
+		}
+	}
 }
