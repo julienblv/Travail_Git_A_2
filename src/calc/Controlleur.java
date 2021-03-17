46b16582664f4b59c7606c5cb95b0c2d92758ec5
julienblv <julien.blavette31@gmail.com>
2021-03-17 17:05:35 +0100

    Ajout de la soustraction, multiplication et division_Julien

--- src/calc/Controlleur.java
+++ src/calc/Controlleur.java
@@ -14,6 +14,9 @@ public class Controlleur {
 	double operant_Deux;
 	
 	boolean addition ;
+	boolean soustraction ;
+	boolean multiplication;
+	boolean division;
 	
 	Controlleur(JTextField _champDeSaisie)
 	{
@@ -29,6 +32,7 @@ public class Controlleur {
 			JButton bouton_click = (JButton) sourceE;
 			String libelle_bouton_click = bouton_click.getText();
 			
+			///////////////////////////////////////// Addition ///////////////////////////////////
 			if (libelle_bouton_click.equals("+"))
 			{
 				// récupération de la variable du champ pour 1er chiffre et affichage dans la barre
@@ -46,16 +50,98 @@ public class Controlleur {
 				
 				champDeSaisie.setText("");
 				// c'est une addition
-				Boolean addition = true;
+				addition = true;
 			}
 			
-			if(addition == true) 
+			///////////////////////////////////////// Soustraction ///////////////////////////////////
+			if (libelle_bouton_click.equals("-"))
+			{
+				// récupération de la variable du champ pour 1er chiffre et affichage dans la barre
+				String texte = champDeSaisie.getText();
+				texte += libelle_bouton_click;
+				texte = texte.replace("-", "");
+				String texte2 = texte.replace("=", "");
+				
+				//parsing
+				double num1 = Double.parseDouble(texte);
+				double num2 = Double.parseDouble(texte2);
+				// Utilisation du modele
+				modele.setNombre1(num1);
+				modele.setNombre2(num2);
+				
+				champDeSaisie.setText("");
+				// c'est une addition
+				soustraction = true;
+			}
+			
+			if(soustraction == true) 
 			{
 				operant_Un = modele.getNombre1();
 				operant_Deux = modele.getNombre2();
-				double Res = modele.addition();
+				double Res = modele.soustraction();
 				champDeSaisie.setText("Resultat : "  + Res);
 			}
+			
+			///////////////////////////////////////// Multiplication ///////////////////////////////////
+			if (libelle_bouton_click.equals("*"))
+			{
+				// récupération de la variable du champ pour 1er chiffre et affichage dans la barre
+				String texte = champDeSaisie.getText();
+				texte += libelle_bouton_click;
+				texte = texte.replace("-", "");
+				String texte2 = texte.replace("=", "");
+				
+				//parsing
+				double num1 = Double.parseDouble(texte);
+				double num2 = Double.parseDouble(texte2);
+				// Utilisation du modele
+				modele.setNombre1(num1);
+				modele.setNombre2(num2);
+				
+				champDeSaisie.setText("");
+				// c'est une addition
+				multiplication = true;
+			}
+			
+			if(multiplication == true) 
+			{
+				operant_Un = modele.getNombre1();
+				operant_Deux = modele.getNombre2();
+				double Res = modele.multiplication();
+				champDeSaisie.setText("Resultat : "  + Res);
+			}
+			
+			///////////////////////////////////////// Division ///////////////////////////////////
+			if (libelle_bouton_click.equals("/"))
+			{
+				// récupération de la variable du champ pour 1er chiffre et affichage dans la barre
+				String texte = champDeSaisie.getText();
+				texte += libelle_bouton_click;
+				texte = texte.replace("-", "");
+				String texte2 = texte.replace("=", "");
+
+				//parsing
+				double num1 = Double.parseDouble(texte);
+				double num2 = Double.parseDouble(texte2);
+				// Utilisation du modele
+				modele.setNombre1(num1);
+				modele.setNombre2(num2);
+
+				champDeSaisie.setText("");
+				// c'est une addition
+				division = true;
+			}
+
+			if(division == true) 
+			{
+				operant_Un = modele.getNombre1();
+				operant_Deux = modele.getNombre2();
+				double Res = modele.multiplication();
+				champDeSaisie.setText("Resultat : "  + Res);
+			}
+			
+			
+			
 		}
 	}
 }
