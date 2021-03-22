c8d0d9d0cde1a67891e49696960ba9791b374d05
hadia <elbouzekraoui.hadia@gmail.com>
2021-03-22 16:12:12 +0100

    Creation de la Jframe: Ajout d'un Jpanel et d'un JtextField

--- src/calc/Vue.java
+++ src/calc/Vue.java
@@ -1,5 +1,31 @@
 package calc;
 
-public class Vue {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class Vue 
+{
+	public static void main(String[] args)
+	{
+		//------------JFrame------------------------------------
+		JFrame calculette = new JFrame();
+		calculette.setTitle("Calculette");
+		calculette.setSize(1000,500);
+		calculette.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		//JPanel
+		JPanel panelNum =new JPanel();
+		
+		//-----------JtextFields--------------------------------
+		JTextField champDeSaisie = new JTextField(30);
+		champDeSaisie.setColumns(10); 
+		champDeSaisie.setBounds(50,1000, 200, 30);
+		panelNum.add(champDeSaisie);
+		
+		calculette.add(panelNum);
+		calculette.setVisible(true);
+	}
+	
 }
