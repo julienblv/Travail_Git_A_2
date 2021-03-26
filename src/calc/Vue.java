1a38cd45923de40453c3833151543b6a28e523fa
hadia <elbouzekraoui.hadia@gmail.com>
2021-03-26 11:07:57 +0100

    changement de l'architecture

--- src/calc/Vue.java
+++ src/calc/Vue.java
@@ -11,93 +11,53 @@ import javax.swing.SpringLayout;
 
 public class Vue 
 {
-	
+	Modele modele;
+	JPanel pnl =new JPanel();
+	JPanel panelNum =new JPanel();
+	JPanel panelOperateur =new JPanel();
+
+	JTextField champDeSaisie;
+
+	JButton btn1 ;
+	JButton btn2 ;
+	JButton btn3 ;
+	JButton btn4 ;
+	JButton btn5 ;
+	JButton btn6 ;
+	JButton btn7 ;
+	JButton btn8 ;
+	JButton btn9 ;
+	JButton btn0 ;
+
+	JButton addition ;
+	JButton soustraction;
+	JButton division;
+	JButton multiplication ;
+	JButton resultat ;
+
+	Vue(Modele _modele){
+		modele = _modele;
+
+		btn1 = new JButton("1");
+		btn2 = new JButton("2");
+		btn3 = new JButton("3");
+		btn4 = new JButton("4");
+		btn5 = new JButton("5");
+		btn6 = new JButton("6");
+		btn7 = new JButton("7");
+		btn8 = new JButton("8");
+		btn9 = new JButton("9");
+		btn0 = new JButton("0");
+
+		addition = new JButton("+");
+		soustraction = new JButton("-");
+		division = new JButton("/");
+		multiplication = new JButton("*");
+		resultat = new JButton("=");
+	}
 	public static void main(String[] args)
 	{
-		//--------------JFrame-----------------------------------
-		JFrame calculette = new JFrame();
-		calculette.setTitle("Calculette");
-		calculette.setSize(510,150);
-		calculette.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//--------Determination du laout de la Frame---------------
-		SpringLayout springLayout = new SpringLayout();  
-		calculette.setLayout(springLayout);
-		
-		//--------1er Panel: JTextField --------------
-		JPanel pnl =new JPanel();
-		pnl.setOpaque(true);
-		pnl.setBackground(Color.pink);
-		pnl.setPreferredSize(new Dimension(500, 30));
-		
-		JTextField champDeSaisie = new JTextField(30);
-		champDeSaisie.setColumns(35); 
-		pnl.add(champDeSaisie);
-		
-		// ajout du panel à la Frame
-		calculette.add(pnl);
-		
-		//--------2eme Panel: Panel de nombre--------------
-		JPanel panelNum =new JPanel();
-		panelNum.setOpaque(true);
-		panelNum.setPreferredSize(new Dimension(250, 100));
-		panelNum.setBackground(Color.YELLOW);
-		
 
-		JButton btn1 = new JButton("1");
-		JButton btn2 = new JButton("2");
-		JButton btn3 = new JButton("3");
-		JButton btn4 = new JButton("4");
-		JButton btn5 = new JButton("5");
-		JButton btn6 = new JButton("6");
-		JButton btn7 = new JButton("7");
-		JButton btn8 = new JButton("8");
-		JButton btn9 = new JButton("9");
-		JButton btn0 = new JButton("0");
-		
-		
-		panelNum.add(btn1);
-		panelNum.add(btn2);
-		panelNum.add(btn3);
-		panelNum.add(btn4);
-		panelNum.add(btn5);
-		panelNum.add(btn6);
-		panelNum.add(btn7);
-		panelNum.add(btn8);
-		panelNum.add(btn9);
-		panelNum.add(btn0);
-		
-		//Ajout du panel à la Frame
-		calculette.add(panelNum);
-		
-		//--------2eme Panel: Panel des Opérateurs--------------
-		JPanel panelOperateur =new JPanel();
-		panelOperateur.setOpaque(true);
-		panelOperateur.setBackground(Color.green);
-		panelOperateur.setPreferredSize(new Dimension(250, 100));
-		
-		JButton addition = new JButton("+");
-		JButton soustraction = new JButton("-");
-		JButton division = new JButton("/");
-		JButton multiplication = new JButton("*");
-		JButton resultat = new JButton("=");
-		
-		panelOperateur.add(addition);
-		panelOperateur.add(soustraction);
-		panelOperateur.add(multiplication);
-		panelOperateur.add(division);
-		panelOperateur.add(resultat);
-		
-		//Ajout du panel à la Frame
-		calculette.add(panelOperateur);
-		
-		//definition des Layout
-		springLayout.putConstraint(SpringLayout.NORTH,panelNum ,0,SpringLayout.SOUTH, pnl);
-		springLayout.putConstraint(SpringLayout.NORTH,panelOperateur ,0,SpringLayout.SOUTH, pnl);
-		springLayout.putConstraint(SpringLayout.WEST,panelOperateur ,0,SpringLayout.EAST, panelNum);
-		
-		// on rend visible la Frame
-		calculette.setVisible(true);
 	}
-	
+
 }
